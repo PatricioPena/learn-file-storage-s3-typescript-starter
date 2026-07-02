ef80ba346517cc8e66953f7a73981bcb2de600bf
Patricio <patricio17po@gmail.com>
2026-07-02 11:13:17 -0600

    CH1-L8

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,9 +1,26 @@
 import { existsSync, mkdirSync } from "fs";
 
 import type { ApiConfig } from "../config";
+import path from 'node:path';
 
 export function ensureAssetsDir(cfg: ApiConfig) {
   if (!existsSync(cfg.assetsRoot)) {
     mkdirSync(cfg.assetsRoot, { recursive: true });
   }
+}
+
+export function getAssetDiskPath(cfg: ApiConfig, filename: string){
+  const result = path.join(cfg.assetsRoot, filename);
+  return result;
+}
+
+export function getAssetURL(cfg: ApiConfig, filename: string){
+  const result = `http://localhost:${cfg.port}/assets/${filename}`
+  return result
+}
+
+export function mediaTypeToExt(mediaType: string): string {
+  const parts = mediaType.split("/");
+  if (parts[1] === "jpeg") return ".jpg";
+  return `.${parts[1]}`;
 }
--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,7 +4,7 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import path from 'node:path';
+import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -15,32 +15,38 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
+  const videoMetaData = getVideo(cfg.db, videoId);
+  if (!videoMetaData) {
+    throw new NotFoundError("Video was not found")
+  }
+  if (videoMetaData.userID != userID) {
+    throw new UserForbiddenError("This user have no access to this video");
+  }
+
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   //  TODO: implement the upload here
   const result = await req.formData();
   const thumbnail = result.get("thumbnail");
-  if(!(thumbnail instanceof File)){
+  if (!(thumbnail instanceof File)) {
     throw new BadRequestError("Invalid thumbnail");
   }
   const MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
-  if(thumbnail.size > MAX_UPLOAD_SIZE){
+  if (thumbnail.size > MAX_UPLOAD_SIZE) {
     throw new BadRequestError("Thumbnail file size is too heavy");
   }
-  const thumbnailData = await thumbnail.arrayBuffer();
-  
-  const videoMetaData =  getVideo(cfg.db, videoId);
-  if(!videoMetaData){
-    throw new NotFoundError("Video was not found")
-  }
-  if(videoMetaData.userID != userID){
-    throw new UserForbiddenError("This user have no access to this video");
+
+  const mediaType = thumbnail.type;
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Invalid file type. Only JPEG or PNG allowed.");
   }
 
-  const ext = thumbnail.type.split("/")[1]
-  const newDataFilePath = path.join(cfg.assetsRoot, `${videoId}.${ext}`)
-  await Bun.write(newDataFilePath, thumbnailData)
-  videoMetaData.thumbnailURL = `http://localhost:${cfg.port}/assets/${videoId}.${ext}`;
+
+  const ext = mediaTypeToExt(mediaType);
+  const filename = `${videoId}${ext}`;
+  const newDataFilePath = getAssetDiskPath(cfg, filename);
+  await Bun.write(newDataFilePath, thumbnail);
+  videoMetaData.thumbnailURL = getAssetURL(cfg, filename);
   updateVideo(cfg.db, videoMetaData);
 
   return respondWithJSON(200, videoMetaData);
